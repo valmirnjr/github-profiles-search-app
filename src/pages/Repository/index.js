ecbf6e0716b4a71b1d05ce4a16250d07100e3e4c
valminjr <junior.valmir6@gmail.com>
2019-08-24 13:16:11 -0300

    update: webview activity indicator

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
 import { WebView } from "react-native-webview";
 import PropTypes from "prop-types";
+
+import { View } from "./styles";
+
+const loadingView = () => (
+  <View>
+    <ActivityIndicator color="#7159c1" size="large" justifyContent="center" />
+  </View>
+);
 
 export default function Repository({ navigation }) {
   const repo = navigation.getParam("repo");
 
-  return <WebView source={{ uri: repo.html_url }} style={{ flex: 1 }} />;
+  return (
+    <WebView
+      source={{ uri: repo.html_url }}
+      style={{ flex: 1 }}
+      startInLoadingState
+      renderLoading={loadingView}
+    />
+  );
 }
 
 Repository.propTypes = {
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
-
-export const Header = styled.View``;
+export const View = styled.View`
+  flex: 1;
+  margin: auto;
+`;
